deb1db1d613e955c4b3ca721ea8a656069428336
Jarne Schreppers <r0711338@ucll.be>
2021-12-01 14:57:51 +0100

    SandwichOverviewPane Finished

--- src/model/database/BroodjesInMemory.java
+++ src/model/database/BroodjesInMemory.java
@@ -6,10 +6,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class BroodjesInMemory {
     private Map<String, Broodje> broodjesDB = new HashMap<>();
--- src/view/panels/SandwichOverviewPane.java
+++ src/view/panels/SandwichOverviewPane.java
@@ -14,7 +14,9 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import model.Beleg;
 import model.Broodje;
+import model.database.BelegInMemory;
 import model.database.BroodjesInMemory;
 
 import java.util.ArrayList;
@@ -25,21 +27,59 @@ public class SandwichOverviewPane extends GridPane{
 	//private TableView<Speler> table;
 	private TableView<Broodje> table = new TableView<>();
 	BroodjesInMemory broodjes = new BroodjesInMemory();
-	List<Broodje> list = new ArrayList<Broodje>(broodjes.getBroodjesDB().values());
-	ObservableList<Broodje> broodjeObservableList;
+	List<Broodje> list = new ArrayList<>(broodjes.getBroodjesDB().values());
+	ObservableList<Broodje> broodjeObservableList =  FXCollections.observableList(list);
+
+	private TableView<Beleg> belegTableView = new TableView<>();
+	BelegInMemory beleg = new BelegInMemory();
+	List<Beleg> list2 = new ArrayList<>(beleg.getBelegDB().values());
+	ObservableList<Beleg> belegObservableList =  FXCollections.observableList(list2);
 	
 	
 	public SandwichOverviewPane() {
+		//Verwijderen als inlezen werkt!
+		Broodje broodje = new Broodje("kaas", 0.30,1,2);
+		List<Broodje> broodjes1 = new ArrayList();
+		broodjes1.add(broodje);
+		ObservableList<Broodje> observers = FXCollections.observableList(broodjes1);
+		System.out.println("Print werkt");
+		System.out.println(observers.get(0).toString());
+
+		//Verwijderen als inlezen werkt!
+		Beleg beleg = new Beleg("kaas", 30,10,2);
+		List<Beleg> beleg1 = new ArrayList();
+		beleg1.add(beleg);
+		ObservableList<Beleg> observers1 = FXCollections.observableList(beleg1);
+		System.out.println("Print werkt");
+		System.out.println(beleg1.get(0).toString());
+
 		this.setPadding(new Insets(5, 5, 5, 5));
         this.setVgap(5);
         this.setHgap(5);        
 		this.add(new Label("Broodjes:"), 0, 0, 1, 1);
-		ListView<String> listView = new ListView<>();
-		listView.getItems().addAll("Action 1", "Action 2", "Action 3", "Action 4");
-		listView.setPrefWidth(260);
-		listView.setMaxWidth(260);
-		listView.setPrefHeight(150);
-		this.setConstraints(listView, 0, 2, 1, 1);
-		this.getChildren().add(listView);
+		table.setItems(observers); //Als inlezen werkt, moeten we observers vervangen door broodjeObservableList
+		TableColumn<Broodje, String> firstNameColumn = new TableColumn<Broodje, String>("Name");
+		TableColumn<Broodje, Double> secondNameColumn = new TableColumn<Broodje, Double>("Sale price");
+		TableColumn<Broodje, Integer> thirdNameColumn = new TableColumn<Broodje, Integer>("Available stock");
+		TableColumn<Broodje, Integer> fourthNameColumn = new TableColumn<Broodje, Integer>("Sold amount");
+		firstNameColumn.setCellValueFactory(new PropertyValueFactory<Broodje,String>("name"));
+		secondNameColumn.setCellValueFactory(new PropertyValueFactory<Broodje, Double>("salePrice"));
+		thirdNameColumn.setCellValueFactory(new PropertyValueFactory<Broodje, Integer>("actualStock"));
+		fourthNameColumn.setCellValueFactory(new PropertyValueFactory<Broodje, Integer>("soldAmount"));
+		table.getColumns().addAll(firstNameColumn,secondNameColumn,thirdNameColumn,fourthNameColumn);
+		this.add(table, 0, 1);
+
+		this.add(new Label("Beleg:"), 1, 0, 1, 1);
+		belegTableView.setItems(observers1); //Als inlezen werkt, moeten we observers1 vervangen door belegObservableList
+		TableColumn<Beleg, String> firstNameColumn1 = new TableColumn<Beleg, String>("Name");
+		TableColumn<Beleg, Double> secondNameColumn1 = new TableColumn<Beleg, Double>("Sale price");
+		TableColumn<Beleg, Integer> thirdNameColumn1 = new TableColumn<Beleg, Integer>("Available stock");
+		TableColumn<Beleg, Integer> fourthNameColumn1 = new TableColumn<Beleg, Integer>("Sold amount");
+		firstNameColumn1.setCellValueFactory(new PropertyValueFactory<Beleg,String>("name"));
+		secondNameColumn1.setCellValueFactory(new PropertyValueFactory<Beleg, Double>("salePrice"));
+		thirdNameColumn1.setCellValueFactory(new PropertyValueFactory<Beleg, Integer>("actualStock"));
+		fourthNameColumn1.setCellValueFactory(new PropertyValueFactory<Beleg, Integer>("soldAmount"));
+		belegTableView.getColumns().addAll(firstNameColumn1,secondNameColumn1,thirdNameColumn1,fourthNameColumn1);
+		this.add(belegTableView, 1, 1);
 	}
 }
