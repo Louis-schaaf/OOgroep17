bd88d8d502725df8e84878c31835ce2bb37756ae
Jarne Schreppers <r0711338@ucll.be>
2021-12-27 10:48:31 +0100

    Story 4 WIP

--- src/model/database/BelegDatabase.java
+++ src/model/database/BelegDatabase.java
@@ -57,6 +57,7 @@ public class BelegDatabase {
         List<BelegSoort> beleg = this.getAll();
         for (BelegSoort b : beleg) {
             result.put(b.getName(), b.getActualStock());
+            //TODO
         }
         return result;
     }
